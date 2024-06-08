f9dd42aa50b5fea691277ed8df450c938dcc4b84
Jose Lucas <lucasnba2324@gmail.com>
2024-06-08 00:47:47 -0300

    feat(users devise) Generating and config users for devise and test

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,31 @@
+# spec/requests/pages_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe 'Pages', type: :request do
+  let(:user) { users(:test) }
+
+  describe 'GET /' do
+    context 'when signed in' do
+      before do
+        sign_in user
+        get root_path
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include(user.first_name)
+      end
+    end
+
+    context 'when signed out' do
+      before do
+        get root_path
+      end
+
+      it 'redirects to sign in' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
