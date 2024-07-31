d50d06b8a9963662015733df92852568d67759ce
Jose Lucas <lucasnba2324@gmail.com>
2024-07-31 18:33:36 -0300

    refactor(application  helper spec) fixing spec test help to make it DRY

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,22 +3,18 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe "format_currency" do
+    subject { helper.format_currency(amount) }
+    let(:amount) {1234.56}
     it "formats the currency correctly" do
-      amount = 1234.56
-      formatted_amount = helper.format_currency(amount)
-      expect(formatted_amount).to eq('R$ 1.234,56')
+      is_expected.to eq("R$ 1.234,56")
     end
 
     it "uses the correct unit" do
-      amount = 1234.56
-      formatted_amount = helper.format_currency(amount)
-      expect(formatted_amount).to include('R$')
+      is_expected.to include("R$")
     end
 
     it "uses the correct format" do
-      amount = 1234.56
-      formatted_amount = helper.format_currency(amount)
-      expect(formatted_amount).to match(/R\$ \d{1,3}(\.\d{3})*,\d{2}/)
+      is_expected.to match(/R\$ \d{1,3}(\.\d{3})*,\d{2}/)
     end
   end
 end
