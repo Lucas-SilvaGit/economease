3258ca074c457dfe45d88cd39fa82678073b9c7d
Jose Lucas <lucasnba2324@gmail.com>
2024-07-31 18:23:39 -0300

    fix(application helper spec) fixing double quotes

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,20 +2,20 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe '#format_currency' do
-    it 'formats the currency correctly' do
+  describe "format_currency" do
+    it "formats the currency correctly" do
       amount = 1234.56
       formatted_amount = helper.format_currency(amount)
       expect(formatted_amount).to eq('R$ 1.234,56')
     end
 
-    it 'uses the correct unit' do
+    it "uses the correct unit" do
       amount = 1234.56
       formatted_amount = helper.format_currency(amount)
       expect(formatted_amount).to include('R$')
     end
 
-    it 'uses the correct format' do
+    it "uses the correct format" do
       amount = 1234.56
       formatted_amount = helper.format_currency(amount)
       expect(formatted_amount).to match(/R\$ \d{1,3}(\.\d{3})*,\d{2}/)
