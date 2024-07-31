c2585eb2bd7930e83361d439d131ba07a83a0fbc
Jose Lucas <lucasnba2324@gmail.com>
2024-07-31 20:03:28 -0300

    refactor(account spec) refactoring account spec tests to make it DRY

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,31 +2,47 @@ require 'rails_helper'
 
 RSpec.describe Account, type: :model do
   let(:user) { create(:user) }
-  let(:account) { create(:account, user: user) }
+  let(:account) { build(:account, user: user) }
 
-  describe 'validations' do
-    it 'validates presence of name' do
-      account.name = nil
-      expect(account).not_to be_valid
-      expect(account.errors[:name]).to include("can't be blank")
+  describe "validations" do
+    context "when name is not present" do
+      before { account.name = nil}
 
-      account.name = 'Savings'
-      account.valid?
-      expect(account.errors[:name]).to be_empty
+      it "is not valid" do
+        is_expected.not_to be_valid
+      end
+
+      it "returns an error message" do
+        account.valid?
+        expect(account.errors[:name]).to include("can't be blank")
+      end
     end
 
+    context "whe name is present" do
+      before { account.name = "Savings"}
+
+      it 'is valid' do
+        expect(account).to be_valid
+      end
+
+      it "does not return an error message" do
+        account.valid?
+        expect(account.errors[:name]).to be_empty
+      end
+    end
   end
 
-  describe 'associations' do
-    it 'belongs to a user' do
-      expect(account.user).to eq(user)
+  describe "associations" do
+    context "has associations" do
+      it "belongs to a user" do
+        expect(account.user).to eq(user)
+      end
     end
   end
 
   describe 'creating an account' do
     context 'with valid parameters' do
       it 'creates a new account successfully' do
-        account = build(:account, name: 'Savings', balance: 1000, user: user)
         expect(account).to be_valid
         account.save
         expect(Account.last).to eq(account)
@@ -36,50 +52,65 @@ RSpec.describe Account, type: :model do
     context 'with invalid parameters' do
       it 'does not create an account without a name' do
         account.name = nil
-        expect(account).not_to be_valid
+        is_expected.not_to be_valid
       end
 
       it 'does not create an account with a negative balance' do
         account.balance = -100
-        expect(account).not_to be_valid
+        is_expected.not_to be_valid
       end
 
       it 'does not create an account with a non-numeric balance' do
         account.balance = 'one hundred'
-        expect(account).not_to be_valid
+        is_expected.not_to be_valid
       end
     end
   end
 
   describe 'calculating partial balance' do
-    context "should be a success" do
+    let(:transaction1) { create(:transaction, account: account, amount: transaction_amount1, transaction_type: transaction_type1) }
+    let(:transaction2) { create(:transaction, account: account, amount: transaction_amount2, transaction_type: transaction_type2) }
+
+    before do
+      transaction1
+      transaction2
+    end
+
+    context 'when calculations are correct' do
+      let(:transaction_amount1) { 1000 }
+      let(:transaction_type1) { 'income' }
+      let(:transaction_amount2) { 300 }
+      let(:transaction_type2) { 'expense' }
+
       it 'calculates the partial balance correctly' do
-        transaction1 = create(:transaction, account: account, amount: 1000, transaction_type: 'income')
-        transaction2 = create(:transaction, account: account, amount: 300, transaction_type: 'expense')
-
         partial_balance = account.partial_balance
-
         expect(partial_balance).to eq(700)
       end
     end
 
-    context "should be a fail" do
-      it 'calculate the partial balance incorrectly' do
-        transaction1 = create(:transaction, account: account, amount: 800, transaction_type: 'income')
-        transaction2 = create(:transaction, account: account, amount: 300, transaction_type: 'expense')
+    context 'when calculations are incorrect' do
+      let(:transaction_amount1) { 800 }
+      let(:transaction_type1) { 'income' }
+      let(:transaction_amount2) { 300 }
+      let(:transaction_type2) { 'expense' }
 
+      it 'calculates the partial balance incorrectly' do
         partial_balance = account.partial_balance
-
         expect(partial_balance).not_to eq(700)
       end
     end
   end
 
   describe 'callbacks' do
-    it 'sets default balance before validation' do
-      new_account = build(:account, name: 'Checking', user: user, balance: nil)
+    let(:new_account) { build(:account, name: 'Checking', user: user, balance: nil) }
+
+    before do
       new_account.valid?
+    end
+
+    it 'sets default balance before validation' do
       expect(new_account.balance).to eq(0)
     end
   end
+
 end
