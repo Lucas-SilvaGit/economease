5609187c934b71bbe6d731cdbddb3ba1bae42da7
Jose Lucas <lucasnba2324@gmail.com>
2024-08-01 19:55:29 -0300

    fix(indentation) fixing indentation and add blank line

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,10 +1,12 @@
 # spec/helpers/application_helper_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe "format_currency" do
     subject { helper.format_currency(amount) }
     let(:amount) {1234.56}
+
     it "formats the currency correctly" do
       is_expected.to eq("R$ 1.234,56")
     end
