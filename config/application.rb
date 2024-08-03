d1fa126ea73e42e30ad025df0453322d3871cf27
Jose Lucas <lucasnba2324@gmail.com>
2024-08-03 01:42:34 -0300

    fix(double quotes) Fixing double quotes

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Economease
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.default_locale = :'pt-BR'
+    config.i18n.default_locale = :"pt-BR"
     config.i18n.available_locales = %i[en pt-BR]
 
     # Don't generate system test files.
