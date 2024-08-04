47b4596e9b01aa18054cbdaf093f415a2d08bf15
Jose Lucas <lucasnba2324@gmail.com>
2024-08-04 00:02:11 -0300

    refactor(category spec) Refactoring category spec tests and fixing double quotes

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,77 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-  let(:user) { create(:user) }
-  let(:goal) { create(:goal, user: user) }
+  context "validations" do
+    let(:user) { create(:user) }
+    let(:goal) { build(:goal, user: user) }
 
-  describe 'validations' do
-    it 'validates presence of name' do
-      goal.name = nil
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:target_amount) }
+
+    it "validates numericality of target_amount" do
+      goal.name = "Valid Goal"
+      goal.target_amount = -1
+
       expect(goal).not_to be_valid
-      expect(goal.errors[:name]).to include("can't be blank")
-
-      goal.name = 'Vacation'
-      goal.valid?
-      expect(goal.errors[:name]).to be_empty
-    end
-
-    it 'validates presence of target_amount' do
-      goal.target_amount = nil
-      expect(goal).not_to be_valid
-      expect(goal.errors[:target_amount]).to include("can't be blank")
-
-      goal.target_amount = 5000
-      goal.valid?
-      expect(goal.errors[:target_amount]).to be_empty
-    end
-
-    it 'validates numericality of target_amount greater than 0' do
-      goal.target_amount = -100
-      expect(goal).not_to be_valid
-      expect(goal.errors[:target_amount]).to include("must be greater than 0")
-
-      goal.target_amount = 5000
-      goal.valid?
-      expect(goal.errors[:target_amount]).to be_empty
+      expect(goal.errors[:target_amount]).to include("deve ser maior que 0")
     end
   end
 
-  describe 'associations' do
-    it 'belongs to a user' do
-      expect(goal.user).to eq(user)
-    end
-  end
-
-  describe 'creating a goal' do
-    context 'with valid parameters' do
-      it 'creates a new goal successfully' do
-        goal = build(:goal, name: 'Vacation', target_amount: 5000, user: user)
-        expect(goal).to be_valid
-        goal.save
-        expect(Goal.last).to eq(goal)
-      end
-    end
-
-    context 'with invalid parameters' do
-      it 'does not create a goal without a name' do
-        goal.name = nil
-        expect(goal).not_to be_valid
-      end
-
-      it 'does not create a goal without a target_amount' do
-        goal.target_amount = nil
-        expect(goal).not_to be_valid
-      end
-
-      it 'does not create a goal with a negative target_amount' do
-        goal.target_amount = -100
-        expect(goal).not_to be_valid
-      end
-
-      it 'does not create a goal with a non-numeric target_amount' do
-        goal.target_amount = 'five thousand'
-        expect(goal).not_to be_valid
-      end
-    end
+  context "associations" do
+    it { should belong_to(:user) }
   end
 end
