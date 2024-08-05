caa8667eb3de228a1c1fabfd09a154adff9375ea
Jose Lucas <lucasnba2324@gmail.com>
2024-08-05 20:40:34 -0300

    refactor(goal test) Refactoring goal test

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -2,19 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
   context "validations" do
-    let(:user) { create(:user) }
-    let(:goal) { build(:goal, user: user) }
-
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:target_amount) }
-
-    it "validates numericality of target_amount" do
-      goal.name = "Valid Goal"
-      goal.target_amount = -1
-
-      expect(goal).not_to be_valid
-      expect(goal.errors[:target_amount]).to include("deve ser maior que 0")
-    end
+    it { should validate_numericality_of(:target_amount).is_greater_than(0) }
   end
 
   context "associations" do
