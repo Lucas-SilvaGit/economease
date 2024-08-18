29facfc88cce5a337b90f2d098896ec418148303
Jose Lucas <lucasnba2324@gmail.com>
2024-08-18 02:13:50 -0300

    feat(transactions filters) Implement show/hide filters functionality
    - Creating _filters partial from the form filters
    - Creating scope in transaction model
    - Creating method set query to the filters
    - Add render filters in transactions index
    - Add some of translations for te filters

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,9 +3,10 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_transaction, only: %i[show edit update destroy]
+  before_action :set_query, only: :index
 
   def index
-    @transactions = Transaction.for_user(current_user)
+    @transactions = @query.for_user(current_user)
   end
 
   def show; end
@@ -73,5 +74,19 @@ class TransactionsController < ApplicationController
 
   def valid_account?(account)
     account && account.user_id == current_user.id
+  end
+
+  def set_query
+    @query = Transaction.all
+    @query = @query.filter_by_description(params[:description]) if params[:description].present?
+    @query = @query.filter_by_transaction_type(params[:transaction_type]) if params[:transaction_type].present?
+    if params[:due_date_start].present? && params[:due_date_end].present?
+      start_date = DateTime.parse(params[:due_date_start])
+      end_date = DateTime.parse(params[:due_date_end])
+      @query = @query.filter_by_due_date_range(start_date, end_date)
+    end
+    @query = @query.filter_by_account_id(params[:account_id]) if params[:account_id].present?
+    @query = @query.filter_by_category_id(params[:category_id]) if params[:category_id].present?
+    @query = @query.filter_by_status(params[:status]) if params[:status].present?
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -15,4 +15,10 @@ class Transaction < ApplicationRecord
   scope :income, -> { where(transaction_type: "income") }
   scope :expense, -> { where(transaction_type: "expense") }
   scope :for_user, ->(user) { joins(:account).where(accounts: { user_id: user.id }).order(due_date: :desc) }
+  scope :filter_by_description, ->(description) { where("description LIKE ?", "%#{description}%") }
+  scope :filter_by_transaction_type, ->(transaction_type) { where(transaction_type:) }
+  scope :filter_by_due_date_range, ->(start_date, end_date) { where(due_date: start_date..end_date) }
+  scope :filter_by_account_id, ->(account_id) { where(account_id:) }
+  scope :filter_by_category_id, ->(category_id) { where(category_id:) }
+  scope :filter_by_status, ->(status) { where(status:) }
 end
