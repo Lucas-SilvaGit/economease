d00d5b6089b2ba9a60473fa4f0792c4553870520
Jose Lucas <lucasnba2324@gmail.com>
2024-08-27 19:48:16 -0300

    refactor(transaction query service) Refactoring private method apply due date filter

--- app/services/transactions/transaction_query_service.rb
+++ app/services/transactions/transaction_query_service.rb
@@ -20,55 +20,25 @@ module Transactions
     private
 
     def apply_due_date_filter(query)
-      if @params[:predefined_period].present?
-        case @params[:predefined_period]
-        when "today"
-          start_date = Date.today
-          end_date = Date.today
-        when "yesterday"
-          start_date = Date.yesterday
-          end_date = Date.yesterday
-        when "this_week"
-          start_date = Date.today.beginning_of_week
-          end_date = Date.today.end_of_week
-        when "last_week"
-          start_date = 1.week.ago.beginning_of_week
-          end_date = 1.week.ago.end_of_week
-        when "last_7_days"
-          start_date = 7.days.ago.to_date
-          end_date = Date.today
-        when "current_month"
-          start_date = Date.today.beginning_of_month
-          end_date = Date.today.end_of_month
-        when "last_30_days"
-          start_date = 30.days.ago.to_date
-          end_date = Date.today
-        when "current_year"
-          start_date = Date.today.beginning_of_year
-          end_date = Date.today.end_of_year
-        when "last_3_months"
-          start_date = 3.months.ago.beginning_of_month
-          end_date = Date.today
-        when "last_6_months"
-          start_date = 6.months.ago.beginning_of_month
-          end_date = Date.today
-        when "last_month"
-          start_date = 1.month.ago.beginning_of_month
-          end_date = 1.month.ago.end_of_month
-        else
-          start_date = Time.zone.today - 30.days
-          end_date = Time.zone.today
-        end
-        query = query.where(due_date: start_date..end_date)
-      elsif @params[:due_date_start].present? && @params[:due_date_end].present?
-        start_date = DateTime.parse(@params[:due_date_start])
-        end_date = DateTime.parse(@params[:due_date_end])
-        query = query.filter_by_due_date_range(start_date, end_date)
-      else
-        start_date = Time.zone.today - 30.days
-        end_date = Time.zone.today
+      predefined_periods = {
+        "today" => -> { [Time.zone.today, Time.zone.today] },
+        "yesterday" => -> { [Time.zone.yesterday, Time.zone.yesterday] },
+        "this_week" => -> { [Time.zone.today.beginning_of_week, Time.zone.today.end_of_week] },
+        "last_week" => -> { [1.week.ago.beginning_of_week, 1.week.ago.end_of_week] },
+        "last_7_days" => -> { [7.days.ago.to_date, Time.zone.today] },
+        "current_month" => -> { [Time.zone.today.beginning_of_month, Time.zone.today.end_of_month] },
+        "last_30_days" => -> { [30.days.ago.to_date, Time.zone.today] },
+        "current_year" => -> { [Time.zone.today.beginning_of_year, Time.zone.today.end_of_year] },
+        "last_3_months" => -> { [3.months.ago.beginning_of_month, Time.zone.today] },
+        "last_6_months" => -> { [6.months.ago.beginning_of_month, Time.zone.today] },
+        "last_month" => -> { [1.month.ago.beginning_of_month, 1.month.ago.end_of_month] }
+      }
+
+      if @params[:predefined_period].present? && predefined_periods.key?(@params[:predefined_period])
+        start_date, end_date = predefined_periods[@params[:predefined_period]].call
         query = query.where(due_date: start_date..end_date)
       end
+
       query
     end
   end
