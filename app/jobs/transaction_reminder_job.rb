1becefeb0a14aa3925a0f69589fb35340a434992
Jose Lucas <lucasnba2324@gmail.com>
2024-08-29 23:05:59 -0300

    feat(Transaction reminder job) Add methodo to check transactions near the due_date

--- app/jobs/transaction_reminder_job.rb
+++ app/jobs/transaction_reminder_job.rb
@@ -4,6 +4,8 @@ class TransactionReminderJob < ApplicationJob
   queue_as :mailers
 
   def perform(transaction)
-    TransactionMailer.transaction_reminder(transaction).deliver_now
+    return unless transaction.due_date == 3.days.from_now.to_date
+
+    TransactionMailer.transaction_reminder(transaction).deliver_later
   end
 end
