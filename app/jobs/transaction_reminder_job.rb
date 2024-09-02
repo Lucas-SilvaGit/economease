c0ce1fc89219db27dc4d5c1801ee83b55771e3b5
Jose Lucas <lucasnba2324@gmail.com>
2024-09-02 20:30:50 -0300

    fix(transaction job) adding iteration over transactions

--- app/jobs/transaction_reminder_job.rb
+++ app/jobs/transaction_reminder_job.rb
@@ -3,9 +3,10 @@
 class TransactionReminderJob < ApplicationJob
   queue_as :mailers
 
-  def perform(transaction)
-    return unless transaction.due_date == 3.days.from_now.to_date
-
-    TransactionMailer.transaction_reminder(transaction).deliver_later
+  def perform
+    transactions = Transaction.where(due_date: 3.days.from_now.to_date)
+    transactions.each do |transaction|
+      TransactionMailer.transaction_reminder(transaction).deliver_later
+    end
   end
 end
