fa5f27c910325ffb6944153cd70b1ce5cdf0b110
Jose Lucas <lucasnba2324@gmail.com>
2024-09-13 20:52:09 -0300

    refactor(accounts): encapsulate balance calculation and add error handling
    - Made `calculate_balance` method private to encapsulate logic.
    - Added exception handling in `call` method to manage errors and provide user-friendly messages.
    - Introduced `handle_error` method to log errors and return a message for the frontend.

--- app/services/accounts/update_balance_service.rb
+++ app/services/accounts/update_balance_service.rb
@@ -8,16 +8,23 @@ module Accounts
 
     def call
       calculate_balance
+    rescue StandardError => e
+      handle_error(e)
     end
+
+    private
 
     def calculate_balance
       total_income = Transactions::CalculateTotalIncomeService.new(@account.id).call
-
       total_expense = Transactions::CalculateTotalExpenseService.new(@account.id).call
-
       calculated_balance = total_income - total_expense
 
       @account.update!(balance: calculated_balance)
+    end
+
+    def handle_error(exception)
+      Rails.logger.error("Failed to update balance: #{exception.message}")
+      I18n.t("activerecord.errors.balance.update_failed")
     end
   end
 end
